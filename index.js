83c802dfd8250c7aa13a498c6a66590569e3c101
Pedro Baptista Campos <106570952+Pedrobcampos20@users.noreply.github.com>
2023-12-02 21:14:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function projetos() {
+    let modal = document.querySelector(".modal_projetos");
+ 
+ 
+    modal.style.display = "block";
+ }
+ 
+ function fechar1() {
+ 
+    let modal = document.querySelector(".modal_projetos");
+ 
+ 
+    modal.style.display = "none";
+}
+
+
+function diplomas() {
+    let modal = document.querySelector(".modal_diplomas");
+ 
+ 
+    modal.style.display = "block";
+}
+ 
+function fechar2() {
+ 
+    let modal = document.querySelector(".modal_diplomas");
+ 
+ 
+    modal.style.display = "none";
+}
